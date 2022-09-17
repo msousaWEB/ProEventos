d9641eedbbc5528fe361b0d2d9b1cdfa238c6917
msousaWEB <msousa.dev@outlook.com>
2022-09-17 15:46:41 -0300

    Criando model do Evento e testando rotas.

--- Backend/src/ProEventos.API/Controllers/EventoController.cs
+++ Backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ProEventos.API.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,15 +13,60 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
+        public IEnumerable<Evento> _evento = new Evento [] {
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .Net 5",
+                Local = "Campinas",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemUrl = "foto.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "React 11 e .Net 5",
+                Local = "São Paulo",
+                Lote = "5º Lote",
+                QtdPessoas = 1000,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemUrl = "logo.png"
+            }
+        };
+
         public EventoController()
         {
             
         }
 
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-            return "value";
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"put = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"delete = {id}";
         }
     }
 }
